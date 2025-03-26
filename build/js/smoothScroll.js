9fa4c0b4e5c0bda16f51700e1bff2a372de864bc
GeorgeDash <georgi.milen.zhelev@gmail.com>
2025-03-26 13:17:17 +0200

    Added smooth scrolling with Lenis

---
+++ build/js/smoothScroll.js
@@ -0,0 +1,9 @@
+// Initialize Lenis
+const lenis = new Lenis({
+    autoRaf: true,
+  });
+  
+  // Listen for the scroll event and log the event data
+  lenis.on('scroll', (e) => {
+    console.log(e);
+  });
